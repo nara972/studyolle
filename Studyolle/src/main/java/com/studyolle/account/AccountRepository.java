a8e94d2bc886ce1b3bbbde94c84401d554e544d6
nara972 <68370958+nara972@users.noreply.github.com>
2021-07-08 16:39:41 +0900

    프로젝트 생성

---
+++ Studyolle/src/main/java/com/studyolle/account/AccountRepository.java
@@ -0,0 +1,16 @@
+package com.studyolle.account;
+
+
+import org.springframework.data.jpa.repository.JpaRepository;
+import org.springframework.transaction.annotation.Transactional;
+
+import com.studyolle.domain.Account;
+
+@Transactional(readOnly=true)
+public interface AccountRepository extends JpaRepository<Account, Long> {
+
+	boolean existsByEmail(String email);
+
+    boolean existsByNickname(String nickname);
+
+}
