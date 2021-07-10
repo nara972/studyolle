5d9ad1774fc131f00f18218e9fea14ec0f363fc9
nara972 <68370958+nara972@users.noreply.github.com>
2021-07-11 01:04:15 +0900

    회원가입 인증메일 확인 테스트 및 리팩토링

--- Studyolle/src/test/java/com/studyolle/account/AccountControllerTest.java
+++ Studyolle/src/test/java/com/studyolle/account/AccountControllerTest.java
@@ -9,6 +9,7 @@ import org.springframework.boot.test.mock.mockito.MockBean;
 import org.springframework.mail.SimpleMailMessage;
 import org.springframework.mail.javamail.JavaMailSender;
 import org.springframework.test.web.servlet.MockMvc;
+import org.springframework.transaction.annotation.Transactional;
 
 import com.studyolle.domain.Account;
 
@@ -21,6 +22,9 @@ import static org.springframework.test.web.servlet.request.MockMvcRequestBuilder
 import static org.springframework.test.web.servlet.result.MockMvcResultHandlers.print;
 import static org.springframework.test.web.servlet.result.MockMvcResultMatchers.*;
 
+import org.apache.tomcat.jni.Status;
+
+@Transactional
 @SpringBootTest
 @AutoConfigureMockMvc
 public class AccountControllerTest {
@@ -29,7 +33,39 @@ public class AccountControllerTest {
 	@Autowired AccountRepository accountRepository; 
 	@MockBean JavaMailSender javaMailSender;
 	
+	@DisplayName("인증 메일 확인 - 입력값 오류")
+	@Test
+	void checkEmailToken_with_wrong_input() throws Exception{
+		mockMvc.perform(get("/check-email-token")
+			   .param("token","sfdfsdfes")
+			   .param("email", "email@email.com"))
+		       .andExpect(status().isOk())
+		       .andExpect(model().attributeExists("error"))
+		       .andExpect(view().name("account/checked-email"))
+		       .andExpect(unauthenticated());
+	}
+	
+	@DisplayName("인증 메일 확인 - 입력값 정상")
+    @Test
+    void checkEmailToken() throws Exception {
+        Account account = Account.builder()
+                .email("test@email.com")
+                .password("12345678")
+                .nickname("keesun")
+                .build();
+        Account newAccount = accountRepository.save(account);
+        newAccount.generateEmailCheckToken();
 
+        mockMvc.perform(get("/check-email-token")
+                .param("token", newAccount.getEmailCheckToken())
+                .param("email", newAccount.getEmail()))
+                .andExpect(status().isOk())
+                .andExpect(model().attributeDoesNotExist("error"))
+                .andExpect(model().attributeExists("nickname"))
+                .andExpect(model().attributeExists("numberOfUser"))
+                .andExpect(view().name("account/checked-email"));
+    }
+	
 	@DisplayName("회원 가입 화면 보이는지 테스트")
 	@Test
 	void signUpForm() throws Exception {
