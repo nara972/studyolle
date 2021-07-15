6e347af030acf2313e1c58aaf9702b8875f1ca29
nara972 <68370958+nara972@users.noreply.github.com>
2021-07-16 01:37:48 +0900

    remember login

--- Studyolle/src/main/java/com/studyolle/config/SecurityConfig.java
+++ Studyolle/src/main/java/com/studyolle/config/SecurityConfig.java
@@ -1,17 +1,30 @@
 package com.studyolle.config;
 
+import javax.sql.DataSource;
+
 import org.springframework.boot.autoconfigure.security.servlet.PathRequest;
+import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
 import org.springframework.http.HttpMethod;
 import org.springframework.security.config.annotation.web.builders.HttpSecurity;
 import org.springframework.security.config.annotation.web.builders.WebSecurity;
 import org.springframework.security.config.annotation.web.configuration.EnableWebSecurity;
 import org.springframework.security.config.annotation.web.configuration.WebSecurityConfigurerAdapter;
+import org.springframework.security.web.authentication.rememberme.JdbcTokenRepositoryImpl;
+import org.springframework.security.web.authentication.rememberme.PersistentTokenRepository;
+
+import com.studyolle.account.AccountService;
+
+import lombok.RequiredArgsConstructor;
 
 
-@Configuration
+@Configuration(proxyBeanMethods = false)
 @EnableWebSecurity
+@RequiredArgsConstructor
 public class SecurityConfig extends WebSecurityConfigurerAdapter {
+	
+	private final AccountService accountService;
+	private final DataSource dataSource;
 	
 	@Override
 	public void configure(HttpSecurity http) throws Exception{
@@ -21,17 +34,30 @@ public class SecurityConfig extends WebSecurityConfigurerAdapter {
 		    .mvcMatchers(HttpMethod.GET,"/profile/*").permitAll()
 		    .anyRequest().authenticated();
 		
+		
 		http.formLogin()
 		    .loginPage("/login").permitAll();
 		
 		http.logout()
 		    .logoutSuccessUrl("/");
+		
+		http.rememberMe()
+		   .userDetailsService(accountService)
+		    .tokenRepository(tokenRepository());
+	}
+	
+	@Bean
+	public PersistentTokenRepository tokenRepository() {
+		JdbcTokenRepositoryImpl jdbcTokenRepository=new JdbcTokenRepositoryImpl();
+		jdbcTokenRepository.setDataSource(dataSource);
+		return jdbcTokenRepository;
 	}
 	
 	@Override
 	public void configure(WebSecurity web) throws Exception{
 		web.ignoring()
 		   .mvcMatchers("/node_modules/**")
+		   .antMatchers("/favicon.ico", "/resources/**", "/error")
 		   .requestMatchers(PathRequest.toStaticResources().atCommonLocations());
 	}
 	
