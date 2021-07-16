5bc2a816f440ae10fd96aafe53aba829c1ae114c
nara972 <68370958+nara972@users.noreply.github.com>
2021-07-17 01:44:12 +0900

    update-profile view

---
+++ Studyolle/src/main/java/com/studyolle/settings/Profile.java
@@ -0,0 +1,25 @@
+package com.studyolle.settings;
+
+import com.studyolle.domain.Account;
+
+import lombok.Data;
+
+@Data
+public class Profile {
+	
+	public Profile(Account account) {
+		this.bio=account.getBio();
+		this.url=account.getUrl();
+		this.occupation=account.getOccupation();
+		this.location=account.getLocation();
+	}
+
+	private String bio;
+	
+	private String url;
+	
+	private String occupation;
+	
+	private String location;
+
+}
