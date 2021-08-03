f30d0dfd20a204bf94cab4860ad201b141a3a7ee
nara972 <68370958+nara972@users.noreply.github.com>
2021-08-04 04:03:27 +0900

    스터디 설정-소개 수정

--- Studyolle/src/main/java/com/studyolle/account/validator/AccountController.java
+++ Studyolle/src/main/java/com/studyolle/account/validator/AccountController.java
@@ -89,15 +89,10 @@ public class AccountController {
 	
 	@GetMapping("/profile/{nickname}")
     public String viewProfile(@PathVariable String nickname, Model model, @CurrentAccount Account account) {
-        Account byNickname = accountRepository.findByNickname(nickname);
-        if (nickname == null) {
-            throw new IllegalArgumentException(nickname + "에 해당하는 사용자가 없습니다.");
-        }
-
-        //뷰에 내려지는 이름을 생략했을 때, 타입의 이름(Account)의 camelCase 를 따른다.
-        model.addAttribute(byNickname);
-        model.addAttribute("isOwner", byNickname.equals(account));
-        return "account/profile";
+		 Account accountToView = accountService.getAccount(nickname);
+	     model.addAttribute(accountToView);
+	     model.addAttribute("isOwner", accountToView.equals(account));
+         return "account/profile";
     }
 	
 	@GetMapping("/email-login")
